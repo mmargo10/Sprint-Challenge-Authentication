8b10783b99f0720ce1a74b7f19d85007b85826b7
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-08-17 13:01:20 -0600

    register and login

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const db = require('./database/dbConfig.js');
 
 const { authenticate } = require('./middlewares');
 
@@ -10,11 +14,75 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  db('users')
+  .insert(user)
+  .then(function(ids) {
+    db('users')
+    .where({ id: ids[0] })
+    .first()
+    .then(user => {
+      const token = generateToken(user);
+      res.send(token);
+      res.status(201).json(user);
+    });
+  })
+  .catch(function(error) {
+    res.status(500).json({ error });
+  });
+};
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1h',
+    jwtid: '147963',
+  };
+  return jwt.sign(payload, secret, options);
 }
 
 function login(req, res) {
   // implement user login
-}
+  const credentials = req.body;
+
+  db('users')
+  .where({ username: credentials.username })
+  .first()
+  .then(function(users) {
+    if (user && bcrypt.compareSync(credentials.password, user.password)) {
+
+    } else {
+      return res.status(401).json({ error: 'incorrect credentials'});
+    } 
+  })
+  .catch((function(error) {
+    res.status(500).json({ error });
+  }));
+};
+
+function protected(req, res, next) {
+  const token = req.headers.authorization; 
+
+    if(token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(err) {
+        return res.status(401).json({ error: 'you shall not pass!!' });
+        }
+
+     req.jwtToken = decodedToken;
+        next();
+    });
+  } else {
+      return res.status(401).json({ error: 'you shall not pass!!' });
+  }
+};
 
 function getJokes(req, res) {
   axios
